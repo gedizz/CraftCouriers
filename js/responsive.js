4d6d99433c18db6a2283ee886528fc39ce95e2f9
gedizzletsplay@gmail.com <Duke1@234>
2022-05-25 23:32:38 +0100

    disabled scrolling while in popup

--- js/responsive.js
+++ js/responsive.js
@@ -6,6 +6,49 @@ var screenWidth = window.screen.width
 
 console.log(screenWidth)
 
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = {37: 1, 38: 1, 39: 1, 40: 1};
+
+function preventDefault(e) {
+  e.preventDefault();
+}
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch (e) { }
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+
+//  disable scrolling while in popup
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+//  enable scrolling when out of popup
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
 
 faBar.addEventListener('click', navClicked);
 navPopupExit.addEventListener('click', navExit);
@@ -13,12 +56,14 @@ navPopupExit.addEventListener('click', navExit);
 // Closes the responsive nav popup when the x is clicked
 function navExit() {
     navPopup.style.display = "none";
+    enableScroll()
 }
 
 // when the responsive <a> fa fa-bar is clicked it will open the mobile nav menu
 function navClicked() {
     if (screenWidth < 800) {
         navPopup.style.display = "flex";
+        disableScroll()
     }
 }
 
@@ -30,5 +75,5 @@ for (var i = 0; i < elements.length; i++) {
     } else {
         el.style.display = "flex";
     }
-    
+
 }
--- js/zipcode.js
+++ js/zipcode.js
@@ -8,12 +8,56 @@ var zipCodeForm = document.getElementById("zipcode");
 
 
 
-const zipCodes = ['55126', '55305', '55488', '55487', '55455', '55112', '55311', '55343', 
-'55345', '55369', '55401', '55402', '55403', '55404', '55405', '55406', '55407', '55408', 
-'55409', '55410', '55411', '55412', '55413', '55414', '55415', '55416', '55417', '55418', 
-'55419', '55421', '55422', '55424', '55426', '55427', '55428', '55429', '55430', '55436', 
-'55441', '55442', '55443', '55444', '55445', '55446', '55447', '55454', '55114', '55108', 
-'55113', '55450', '55423', '55435', '55439', '55344', '55346', '55316', '55432'];
+const zipCodes = ['55126', '55305', '55488', '55487', '55455', '55112', '55311', '55343',
+    '55345', '55369', '55401', '55402', '55403', '55404', '55405', '55406', '55407', '55408',
+    '55409', '55410', '55411', '55412', '55413', '55414', '55415', '55416', '55417', '55418',
+    '55419', '55421', '55422', '55424', '55426', '55427', '55428', '55429', '55430', '55436',
+    '55441', '55442', '55443', '55444', '55445', '55446', '55447', '55454', '55114', '55108',
+    '55113', '55450', '55423', '55435', '55439', '55344', '55346', '55316', '55432'];
+
+// left: 37, up: 38, right: 39, down: 40,
+// spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36
+var keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+
+
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+// modern Chrome requires { passive: false } when adding event
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+        get: function () { supportsPassive = true; }
+    }));
+} catch (e) { }
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+//  disable scrolling while in popup
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+//  enable scrolling when out of popup
+function enableScroll() {
+    window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.removeEventListener(wheelEvent, preventDefault, wheelOpt);
+    window.removeEventListener('touchmove', preventDefault, wheelOpt);
+    window.removeEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
 
 // Event listener checking if the zipcode button was clicked
 zipCodeButton.addEventListener('click', buttonClicked);
@@ -24,6 +68,7 @@ zipExitButton2.addEventListener('click', hideElement);
 function hideElement() {
     insideRangeDiv.style.display = "none";
     outsideRangeDiv.style.display = "none";
+    enableScroll()
 }
 
 // When they click the zip code button it will change the style display
@@ -35,13 +80,15 @@ function buttonClicked() {
 
     if (withinRange) {
         insideRangeDiv.style.display = "flex"
-        
+        disableScroll()
+
     } else {
         outsideRangeDiv.style.display = "flex";
+        disableScroll()
     }
 
     // Display last so changes to div are made prior
-   // popupDiv.style.display = "flex";
+    // popupDiv.style.display = "flex";
 }
 
 
