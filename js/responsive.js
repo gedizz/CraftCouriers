d74fa44fada98a025ad9d35e85cc079b73fc2e55
gedizzletsplay@gmail.com <Duke1@234>
2022-05-25 22:00:00 +0100

    finished responsive nav and cleaned up sections

--- js/responsive.js
+++ js/responsive.js
@@ -1,7 +1,26 @@
-elements = document.getElementsByName("normalnav");
-responsiveNav = document.getElementById("responsivenav");
-screenWidth = window.screen.width
+var elements = document.getElementsByName("normalnav");
+var navPopup = document.getElementById("nav-popup");
+var navPopupExit = document.getElementById("nav-popup-exit")
+var faBar = document.getElementById("fapopup");
+var screenWidth = window.screen.width
+
 console.log(screenWidth)
+
+
+faBar.addEventListener('click', navClicked);
+navPopupExit.addEventListener('click', navExit);
+
+// Closes the responsive nav popup when the x is clicked
+function navExit() {
+    navPopup.style.display = "none";
+}
+
+// when the responsive <a> fa fa-bar is clicked it will open the mobile nav menu
+function navClicked() {
+    if (screenWidth < 800) {
+        navPopup.style.display = "flex";
+    }
+}
 
 // Select all normalnav named elements
 for (var i = 0; i < elements.length; i++) {
